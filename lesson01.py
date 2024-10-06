4c4274a553ddaac104a65bcdfdc23c009318d3ea
shalvaaleks <aleksishvilishalva@gmail.com>
2024-10-06 23:36:40 +0400

    second commit

---
+++ lesson01.py
@@ -0,0 +1,17 @@
+name ="shalva"
+surname = "aleksishvili"
+
+print(name)
+print(surname)
+print(name + surname)
+height = "165"
+age = "11"
+knows_games = True
+
+print(name)
+print(surname)
+print(height)
+print(age)
+print(knows_games)
+
+print(name + " " + surname  + " " + height + age + str(knows_games))
