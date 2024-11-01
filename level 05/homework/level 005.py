80cadbba5d659293f9a2784f1c8c78f50abd8753
shalvaaleks <aleksishvilishalva@gmail.com>
2024-11-01 23:58:59 +0400

    second commit

---
+++ level 05/homework/level 005.py
@@ -0,0 +1,26 @@
+number1 = 36
+number2 = 7
+print (number1 + number2)
+print (number1 * number2)
+print (number1 - number2)
+print (number1 / number2)
+
+number1 = 29
+number2 = 6
+print (number1 + 20)
+print (number1 - number2)
+print (number1 * 3)
+print (number1 / number2)
+
+number1 = 120
+number2 = 50
+print (number1 + number2)
+print (number1 - number2)
+print (number1 * number2)
+print (number1 / number2)
+
+number1 = 311
+number2 = 30
+print (number1 + number2)
+print (number1 - number2)
+print (number1 * number2)
