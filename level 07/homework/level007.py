e4510326dac204fc4ff832169e05da188c60bfc0
shalvaaleks <aleksishvilishalva@gmail.com>
2024-11-02 14:53:54 +0400

    second commit

---
+++ level 07/homework/level007.py
@@ -0,0 +1,16 @@
+number1 = 56
+number2 = 23
+print(number1 + number2)
+print(number2 * 20)
+print(number2 - 10)
+print(number1 / 6)
+
+nuber1= 23
+number2= 39
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 * number2 + 12)
+print(number1 + 234)
+print(number1 - number2 +25)
---
+++ level 07/homework/level07.py
@@ -0,0 +1,8 @@
+name = "shalva"
+last_name = "aleksishvili"
+age_input = "11"
+input_country = "georgia"
+enter_city = "tbilisi"
+favourite_car = "lamborgini"
+favourite_color = "green"
+print (name + last_name + age_input + input_country + enter_city + favourite_car + favourite_color)
